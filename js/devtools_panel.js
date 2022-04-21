e5119cdf38411fe7f2cd43546b04716bde7c3a74
Jonathon Black <67678754+JBlack96@users.noreply.github.com>
2022-04-21 10:58:05 +0100

    section 2.3.0 - append request to ui element

--- js/devtools_panel.js
+++ js/devtools_panel.js
@@ -1,5 +1,12 @@
 console.log('connected and ran script');
 
+const element = document.getElementById("devtool-app");
+
 chrome.devtools.network.onRequestFinished.addListener((request) => {
     console.log(request);
+
+    // append request element to ui
+    const impressionEl = document.createElement('div');
+    impressionEl.innerHTML = JSON.stringify(request);
+    element.appendChild(impressionEl);
 });
