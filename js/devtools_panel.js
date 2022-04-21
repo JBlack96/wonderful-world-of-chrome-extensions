84a234f85722fbc25f5ce5098b4135adc489a6b5
Jonathon Black <67678754+JBlack96@users.noreply.github.com>
2022-04-21 11:03:49 +0100

    section 2.4.0 - salecycle impression request

--- js/devtools_panel.js
+++ js/devtools_panel.js
@@ -3,12 +3,17 @@ console.log('connected and ran script');
 const element = document.getElementById("devtool-app");
 
 chrome.devtools.network.onRequestFinished.addListener((request) => {
-    console.log(request);
+    // check for an impression
+    if (request.request.url.includes('i.salecycle.com')) {
+        if (!request.request.postData || !request.request.postData.text) {
+            return;
+        }
 
-    // append request element to ui
-    const impressionEl = document.createElement('div');
-    impressionEl.innerHTML = JSON.stringify(request);
-    element.appendChild(impressionEl);
+        // append request element to ui
+        const impressionEl = document.createElement('div');
+        impressionEl.innerHTML = unescape(request.request.postData.text);
+        element.appendChild(impressionEl);
+    }
 });
 
 document.querySelector('#clearBtn').addEventListener('click', (e) => {
